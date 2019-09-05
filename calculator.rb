082b4b9e43cb8291901ab1213ff3dcdf7b9861cd
Allison Healy <ahealy@Allisons-MacBook-Pro.local>
2019-09-05 14:14:56 -0600

    refactored math type into method call

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,24 @@
 class Calculator
+  def self.be_useful
+    puts "what math do you want"
+    math_type = gets.chomp
+
+    puts "What's your first number?"
+    first_number = gets.chomp.to_i
+
+    puts "What's your second number?"
+    second_number = gets.chomp.to_i
+
+
+    if math_type == "add"
+      puts add(first_number, second_number)
+    end
+
+    if math_type == "subtract"
+      puts subtract(first_number, second_number)
+    end
+  end
+
   def self.subtract(x,y)
     (x-y)
   end
@@ -18,26 +38,28 @@ end
 
 
 
-puts "gimme a number"
- x = gets.chomp.to_i
- puts "gimme another number"
- y = gets.chomp.to_i
-puts Calculator.subtract(x,y)
+# puts "gimme a number"
+#  x = gets.chomp.to_i
+#  puts "gimme another number"
+#  y = gets.chomp.to_i
+# puts Calculator.subtract(x,y)
+#
+# puts "gimme a number"
+#  a = gets.chomp.to_i
+#  puts "gimme another number"
+#  b = gets.chomp.to_i
+# puts Calculator.add(a,b)
+#
+# puts "gimme a number"
+#  f = gets.chomp.to_i
+#  puts "gimme another number"
+#  d = gets.chomp.to_i
+# puts Calculator.multiply(f,d)
+#
+# puts "gimme a number"
+#  z = gets.chomp.to_i
+#  puts "gimme another number"
+#  w = gets.chomp.to_i
+# puts Calculator.divide(z,w)
 
-puts "gimme a number"
- a = gets.chomp.to_i
- puts "gimme another number"
- b = gets.chomp.to_i
-puts Calculator.add(a,b)
-
-puts "gimme a number"
- f = gets.chomp.to_i
- puts "gimme another number"
- d = gets.chomp.to_i
-puts Calculator.multiply(f,d)
-
-puts "gimme a number"
- z = gets.chomp.to_i
- puts "gimme another number"
- w = gets.chomp.to_i
-puts Calculator.divide(z,w)
+Calculator.be_useful
