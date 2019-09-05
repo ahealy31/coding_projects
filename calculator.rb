2220bf2b1890b97a9cc7341b85c12fc6c59273cc
Allison Healy <ahealy@Allisons-MacBook-Pro.local>
2019-09-05 13:38:42 -0600

    Added Calculator

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,43 @@
-def subtract (x,y)
-  puts x-y
+class Calculator
+  def self.subtract(x,y)
+    (x-y)
+  end
+
+  def self.add(a,b)
+    (a+b)
+  end
+
+  def self.multiply(f,d)
+    (f*d)
+  end
+
+  def self.divide(z,w)
+    (z/w)
+  end
 end
 
-subtract(5,3)
+
+
+puts "gimme a number"
+ x = gets.chomp.to_i
+ puts "gimme another number"
+ y = gets.chomp.to_i
+puts Calculator.subtract(x,y)
+
+puts "gimme a number"
+ a = gets.chomp.to_i
+ puts "gimme another number"
+ b = gets.chomp.to_i
+puts Calculator.add(a,b)
+
+puts "gimme a number"
+ f = gets.chomp.to_i
+ puts "gimme another number"
+ d = gets.chomp.to_i
+puts Calculator.multiply(f,d)
+
+puts "gimme a number"
+ z = gets.chomp.to_i
+ puts "gimme another number"
+ w = gets.chomp.to_i
+puts Calculator.divide(z,w)
